8181fd3441adf6a99612fcac0aa6b3b07b913966
landmanbester <lbester@ska.ac.za>
2023-04-14 15:41:32 +0200

    add matplotlib dependency

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ with open("README.md", "r") as fh:
 requirements = [
                 'numpy',
                 'scipy',
+                'matplotlib',
                 'numba',
                 'sympy',
                 'pytest >= 6.2.2',
